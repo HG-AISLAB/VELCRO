6f8d9c6fbe38c699117b6ffe0165b5e604eab360
MinYoung-Lim <52227367+MinYoung-Lim@users.noreply.github.com>
2023-09-21 14:37:09 +0900

    Delete base_model_select directory

--- base_model_select/run_predict.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-chmod 755 yolov5/predict.py
-python yolov5/predict.py --task evaluate --data coco.yaml --img 640 --iou 0.65 --half
--- base_model_select/tests.py
+++
@@ -1,5 +0,0 @@
-import unittest
-import generate_txt_file
-
-class generate_txt_file_test(unittest.TestCase):
-    def test
